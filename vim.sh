93573c350ca7c07c137ce127622bd15bbbca7150
Hugo Rambure <32395687+Nodli@users.noreply.github.com>
2020-04-24 18:42:28 +0200

    Update vim.sh

--- vim.sh
+++ vim.sh
@@ -6,6 +6,10 @@ cd source
 executable_path="./../linux/makerun.sh"
 session_path="./../linux/session.vim"
 
-vim -c ":nnoremap <F5> :w<CR> :!${executable_path}<CR>" \
-    -c ":autocmd VimEnter * if filereadable(\"${session_path}\") | :source ${session_path} | else | :next **/*.h **/*.cpp **/*.c **/*.inl | endif" \
-    -c ":autocmd VimLeave * :mksession! ${session_path}"
+vim -c ":nnoremap <F5> :wa<CR> :!${executable_path}<CR>"                                                                                                                        \
+    -c ":nnoremap <F6> :!<CR>"                                                                                                                                                  \
+    -c ":autocmd VimEnter * if filereadable(\"${session_path}\") | :source ${session_path} | else | :args **/*.h **/*.cpp **/*.inl | :set filetype=cpp | endif"                 \
+    -c ":autocmd VimLeave * :mksession! ${session_path}"                                                                                                                        \
+    -c ":set number"                                                                                                                                                            \
+    -c ":nnoremap <F7> :e %:p:s,.h$,.X123X,:s,.cpp$,.h,:s,.X123X$,.cpp,<CR>"                                                                                                    \
+    -c ":autocmd VimEnter * :b main.cpp | :set filetype=cpp"                                                                                                                    \
